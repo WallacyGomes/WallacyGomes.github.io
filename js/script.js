1df1b023fd2268f69d7a9b5307488203975b179c
WallacyGomes <wg.wallacygomes@gmail.com>
2023-05-28 18:48:17 -0300

    adicionando menu mobile

--- js/script.js
+++ js/script.js
@@ -51,3 +51,74 @@ function activeMenuOpacity(){
 }
 
 activeMenuOpacity();
+
+function activeMenuMobile(){
+  const menuBtn = document.querySelector('.menu-btn');
+  const header = document.querySelector('.header');
+  let btnclicado = true;
+
+  //Função para abrir ou fechar o menu mobile
+  function openMenu(){
+    if(btnclicado === true){
+      const menuContentContainer = document.createElement('div');
+      menuContentContainer.classList.add('menu-content-container');
+      header.appendChild(menuContentContainer);
+      
+      const ulMenuContent = document.createElement('ul');
+      ulMenuContent.classList.add('menu-content');
+      menuContentContainer.appendChild(ulMenuContent);
+      
+      const sobreMenuContent = document.createElement('a');
+      sobreMenuContent.setAttribute('href', '#sobre');
+      const sobreText = document.createTextNode('Sobre');
+      sobreMenuContent.appendChild(sobreText);
+      ulMenuContent.appendChild(sobreMenuContent);
+      
+      const tecnologiasMenuContent = document.createElement('a');
+      tecnologiasMenuContent.setAttribute('href', '#tecnologias')
+      const tecnologiasText = document.createTextNode('Tecnologias');
+      tecnologiasMenuContent.appendChild(tecnologiasText);
+      ulMenuContent.appendChild(tecnologiasMenuContent);
+      
+      const portfolioMenuContent = document.createElement('a');
+      portfolioMenuContent.setAttribute('href', '#portfolio');
+      const portfolioText = document.createTextNode('Portfolio');
+      portfolioMenuContent.appendChild(portfolioText);
+      ulMenuContent.appendChild(portfolioMenuContent);
+      
+      const certificadosMenuContent = document.createElement('a');
+      certificadosMenuContent.setAttribute('href', '#certificados');
+      const certificadosText = document.createTextNode('Certificados')
+      certificadosMenuContent.appendChild(certificadosText);
+      ulMenuContent.appendChild(certificadosMenuContent);
+      
+      const contatoMenuContent = document.createElement('a');
+      contatoMenuContent.setAttribute('href', '#contato');
+      const contatoText = document.createTextNode('Contato');
+      contatoMenuContent.appendChild(contatoText);
+      ulMenuContent.appendChild(contatoMenuContent);
+      
+      btnclicado = false;
+
+    } else{
+      const novaDIV = document.querySelector('.menu-content-container');
+      novaDIV.remove();
+      btnclicado = true;
+    }
+    activeScrollSuave();
+  }
+  menuBtn.addEventListener('click', openMenu);
+
+  //Verificando se a tela não é mais mobile
+  window.addEventListener('resize', () => {
+    const novaDiv = document.querySelector('.menu-content-container');
+    
+    if(window.innerWidth > 600){
+      novaDiv.classList.add('desativado');
+    } else{
+      novaDiv.classList.remove('desativado');
+    }
+  });
+}
+
+activeMenuMobile();
