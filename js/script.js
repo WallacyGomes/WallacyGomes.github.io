dc02af56ddc06624f13bf1c5a46fd33617e7d3a8
WallacyGomes <wg.wallacygomes@gmail.com>
2023-05-30 14:55:10 -0300

    add animação menu hamburguer

--- js/script.js
+++ js/script.js
@@ -40,83 +40,85 @@ function activeScrollSection() {
 
 activeScrollSection();
 
-function activeMenuOpacity(){
+function activeMenuOpacity() {
   const menu = document.querySelector(".js-menuopacity");
 
-  window.addEventListener("scroll", (MenuOpacity) =>  {
+  window.addEventListener("scroll", (MenuOpacity) => {
     const diminuirOpacidade = scrollY > 0;
-    if (diminuirOpacidade) menu.classList.add("ativo")
-    else menu.classList.remove("ativo")
+    if (diminuirOpacidade) menu.classList.add("ativo");
+    else menu.classList.remove("ativo");
   });
 }
 
 activeMenuOpacity();
 
-function activeMenuMobile(){
-  const menuBtn = document.querySelector('.menu-btn');
-  const header = document.querySelector('.header');
+function activeMenuMobile() {
+  const corpo = document.querySelector("body");
+  const menuBtn = document.querySelector(".menu-btn");
+  const header = document.querySelector(".header");
   let btnclicado = true;
 
   //Função para abrir ou fechar o menu mobile
-  function openMenu(){
-    if(btnclicado === true){
-      const menuContentContainer = document.createElement('div');
-      menuContentContainer.classList.add('menu-content-container');
+  function openMenu() {
+    if (btnclicado === true) {
+      const menuContentContainer = document.createElement("div");
+      menuContentContainer.classList.add("menu-content-container");
+      menuBtn.classList.add("aberto");
       header.appendChild(menuContentContainer);
-      
-      const ulMenuContent = document.createElement('ul');
-      ulMenuContent.classList.add('menu-content');
+
+      const ulMenuContent = document.createElement("ul");
+      ulMenuContent.classList.add("menu-content");
       menuContentContainer.appendChild(ulMenuContent);
-      
-      const sobreMenuContent = document.createElement('a');
-      sobreMenuContent.setAttribute('href', '#sobre');
-      const sobreText = document.createTextNode('Sobre');
+
+      const sobreMenuContent = document.createElement("a");
+      sobreMenuContent.setAttribute("href", "#sobre");
+      const sobreText = document.createTextNode("Sobre");
       sobreMenuContent.appendChild(sobreText);
       ulMenuContent.appendChild(sobreMenuContent);
-      
-      const tecnologiasMenuContent = document.createElement('a');
-      tecnologiasMenuContent.setAttribute('href', '#tecnologias')
-      const tecnologiasText = document.createTextNode('Tecnologias');
+
+      const tecnologiasMenuContent = document.createElement("a");
+      tecnologiasMenuContent.setAttribute("href", "#tecnologias");
+      const tecnologiasText = document.createTextNode("Tecnologias");
       tecnologiasMenuContent.appendChild(tecnologiasText);
       ulMenuContent.appendChild(tecnologiasMenuContent);
-      
-      const portfolioMenuContent = document.createElement('a');
-      portfolioMenuContent.setAttribute('href', '#portfolio');
-      const portfolioText = document.createTextNode('Portfolio');
+
+      const portfolioMenuContent = document.createElement("a");
+      portfolioMenuContent.setAttribute("href", "#portfolio");
+      const portfolioText = document.createTextNode("Portfolio");
       portfolioMenuContent.appendChild(portfolioText);
       ulMenuContent.appendChild(portfolioMenuContent);
-      
-      const certificadosMenuContent = document.createElement('a');
-      certificadosMenuContent.setAttribute('href', '#certificados');
-      const certificadosText = document.createTextNode('Certificados')
+
+      const certificadosMenuContent = document.createElement("a");
+      certificadosMenuContent.setAttribute("href", "#certificados");
+      const certificadosText = document.createTextNode("Certificados");
       certificadosMenuContent.appendChild(certificadosText);
       ulMenuContent.appendChild(certificadosMenuContent);
-      
-      const contatoMenuContent = document.createElement('a');
-      contatoMenuContent.setAttribute('href', '#contato');
-      const contatoText = document.createTextNode('Contato');
+
+      const contatoMenuContent = document.createElement("a");
+      contatoMenuContent.setAttribute("href", "#contato");
+      const contatoText = document.createTextNode("Contato");
       contatoMenuContent.appendChild(contatoText);
       ulMenuContent.appendChild(contatoMenuContent);
-      
-      btnclicado = false;
 
-    } else{
-      const novaDIV = document.querySelector('.menu-content-container');
+      btnclicado = false;
+    } else {
+      const novaDIV = document.querySelector(".menu-content-container");
       novaDIV.remove();
+      menuBtn.classList.remove("aberto");
       btnclicado = true;
     }
-    activeScrollSuave();
   }
-  menuBtn.addEventListener('click', openMenu);
+
+  menuBtn.addEventListener("click", openMenu);
 
   //Verificando se a tela não é mais mobile
-  window.addEventListener('resize', () => {
-    const novaDiv = document.querySelector('.menu-content-container');
-    
-    if(window.innerWidth > 600){
-      novaDiv.classList.add('desativado');
-    } else{
-      novaDiv.classList.remove('desativado');
+  window.addEventListener("resize", () => {
+    const novaDiv = document.querySelector(".menu-content-container");
+
+    if (window.innerWidth > 600) {
+      novaDiv.classList.add("desativado");
+    } else {
+      novaDiv.classList.remove("desativado");
     }
   });
 }
